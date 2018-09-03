a950371a5ec021473f31ad559f7b6faadcc1e005
이성호 <seongho051@naver.com>
2018-09-03 20:22:06 +0900

    feat: 임의의 컴포넌트들을 export 시킨다

---
+++ src/Button/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Button extends Component {
+  render() {
+    return (
+      <button>Test</button>
+    );
+  }
+}
---
+++ src/ReadMore/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ReadMore extends Component {
+  render() {
+    return (
+      <button>Test</button>
+    );
+  }
+}
