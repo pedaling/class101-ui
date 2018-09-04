3a76dcfb0417fb3b6db8c7a9df566485d9acd7b8
이성호 <seongho051@naver.com>
2018-09-04 21:50:33 +0900

    test: Spinner의 테스트 코드를 추가한다

---
+++ src/Spinner/index.spec.js
@@ -0,0 +1,11 @@
+import Spinner from './';
+
+describe('Grid', () => {
+  it('값이 존재한다.', () => {
+    expect(Spinner).toBeTruthy();
+  });
+
+  it('스냅샷과 일치한다..', () => {
+    expect(Spinner).toMatchSnapshot();
+  });
+});
