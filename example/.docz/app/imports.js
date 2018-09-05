0030c05eaac0885f6893010ea67f2d779e230186
이성호 <seongho051@naver.com>
2018-09-05 19:44:33 +0900

    docs: gitignore을 변경한다

--- example/.docz/app/imports.js
+++
@@ -1,8 +0,0 @@
-export const imports = {
-  'src/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-index" */ 'src/index.mdx'),
-  'src/components/ProgressBar.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-progress-bar" */ 'src/components/ProgressBar.mdx'),
-  'src/components/Spinner.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-spinner" */ 'src/components/Spinner.mdx'),
-}
