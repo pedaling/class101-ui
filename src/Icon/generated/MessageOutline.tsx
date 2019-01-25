6459303d346f3df9958ca51ef72328fc7e5e7e5b
배현승 <bae-unidev@nate.com>
2019-01-25 14:24:54 +0900

    feat: 아이콘 변경사항을 적용한다

--- src/Icon/generated/MessageOutline.tsx
+++ src/Icon/generated/MessageOutline.tsx
@@ -7,7 +7,7 @@ export default class SvgMessageOutline extends React.PureComponent<IconProps> {
   };
 
   public render() {
-    return <svg width={this.props.size} height={this.props.size} viewBox="0 0 24 24"><path fill={this.props.fillColor} fillRule="evenodd" d="M4 4h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2zm0 2v12h16V6H4zm3.514 2.143L12 10.833l4.486-2.69a1 1 0 1 1 1.028 1.714l-5 3a1 1 0 0 1-1.028 0l-5-3a1 1 0 1 1 1.028-1.714z" /></svg>;
+    return <svg height={this.props.size} width={this.props.size} viewBox="0 0 24 24"><path d="M4 4h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2zm0 2v12h16V6zm3.514 2.143L12 10.833l4.486-2.69a1 1 0 1 1 1.028 1.714l-5 3a1 1 0 0 1-1.028 0l-5-3a1 1 0 1 1 1.028-1.714z" fill="#3e4042" fillRule="evenodd" /></svg>;
   }
 
 }
