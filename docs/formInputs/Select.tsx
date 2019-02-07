ba667b69158842881b10a90024cb2390a49d81da
배현승 <bae-unidev@nate.com>
2019-02-07 22:58:03 +0900

    feat: Select를 추가한다

---
+++ docs/formInputs/Select.tsx
@@ -0,0 +1,12 @@
+import { Select } from '@class101/ui';
+import React from 'react';
+
+type OptionProps = string | { label?: string; disabled?: boolean; value?: string | number | undefined };
+
+interface Props {
+  options?: OptionProps[];
+  value: string | boolean | undefined;
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => any;
+}
+
+export const Component = ({ ...restProps }: Props) => <Select {...restProps} />;
