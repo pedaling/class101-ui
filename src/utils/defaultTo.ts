5e88335364dd1db503867696cdf81449817ed34e
chiabi <chiabi88@gmail.com>
2019-03-06 12:19:01 +0900

    refactor: lodash를 추가하고 누락된 static을 추가한다

--- src/utils/defaultTo.ts
+++
@@ -1,3 +0,0 @@
-export default function defaultTo<T, TDefault>(value: T | null | undefined, defaultValue: TDefault): T | TDefault {
-  return value == null || value !== value ? defaultValue : value;
-}
