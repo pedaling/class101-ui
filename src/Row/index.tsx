d6994a92c8ffac6c76c8c16bd2bda92266b08e46
chiabi <chiabi88@gmail.com>
2019-05-09 05:00:52 +0900

    feat(deprecated): Layout 컴포넌트에서 margin 스타일을 제어하는 속성을 제거한다
    - {element}Attributes 이름 형식으로 HTMLAttributes를 받는 속성을 추가한다
    - mdx문서에 @class101/ui 컴포넌트를 바로import하고 불필요한 컴포넌트는 제거한다
    - GridList.mdx의 Layout import를 수정하면서 GridList도 PureComponent로
    리팩토링한다

--- src/Row/index.tsx
+++ src/Row/index.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 
 import { media } from '../BreakPoints';
 import { HTMLDivProps } from '../interfaces/props';
 
-interface Props extends HTMLDivProps {
-  marginTop?: number | string;
-  marginBottom?: number | string;
+interface Props {
+  className?: string;
+  divAttributes?: HTMLDivProps;
+}
+
+export default class Row extends PureComponent<Props> {
+  public render() {
+    const { children, divAttributes, className } = this.props;
+    return (
+      <StyledDiv className={className} {...divAttributes}>
+        {children}
+      </StyledDiv>
+    );
+  }
 }
 
 const StyledDiv = styled.div<Props>`
@@ -14,16 +25,6 @@ const StyledDiv = styled.div<Props>`
   flex-wrap: wrap;
   justify-content: flex-start;
   align-content: stretch;
-  ${props =>
-    props.marginTop &&
-    `
-      margin-top: ${typeof props.marginTop === 'number' ? `${props.marginTop}px` : props.marginTop};
-    `}
-  ${props =>
-    props.marginBottom &&
-    `
-      margin-bottom: ${typeof props.marginBottom === 'number' ? `${props.marginBottom}px` : props.marginBottom};
-    `}
   margin-right: -12px;
   margin-left: -12px;
   ${media.sm`
@@ -31,10 +32,3 @@ const StyledDiv = styled.div<Props>`
     margin-left: -4px;
   `};
 `;
-
-export default class Row extends React.PureComponent<Props> {
-  public render() {
-    const { children, ...restProps } = this.props;
-    return <StyledDiv {...restProps}>{children}</StyledDiv>;
-  }
-}
--- src/Section/index.tsx
+++ src/Section/index.tsx
@@ -1,18 +1,10 @@
-import React, { ReactNode } from 'react';
-import styled from 'styled-components';
+import React, { PureComponent, ReactNode } from 'react';
 
 import { HTMLDivProps } from '../interfaces/props';
 import Footer from './Footer';
 import Header from './Header';
 
-interface SectionStyleProps {
-  marginTop?: number | string;
-  marginBottom?: number | string;
-  marginLeft?: number | string;
-  marginRight?: number | string;
-}
-
-interface OwnProps {
+interface Props {
   title: ReactNode;
   titleStyle?: any;
   description?: string;
@@ -23,45 +15,46 @@ interface OwnProps {
   target?: string;
   onClick?: () => void;
   children?: ReactNode;
+  className?: string;
+  divAttributes?: HTMLDivProps;
 }
 
-export type SectionProps = OwnProps & SectionStyleProps & HTMLDivProps;
-
-const Section = styled.section<SectionStyleProps>`
-  margin: ${props =>
-    `${props.marginTop || 0}px ${props.marginRight || 0}px ${props.marginBottom || 0}px ${props.marginLeft || 0}`}px;
-`;
-
-export default ({
-  title,
-  titleStyle,
-  description,
-  buttonTitle,
-  buttonStyle,
-  to,
-  href,
-  target,
-  onClick,
-  children,
-  ...restProps
-}: SectionProps) => (
-  <Section {...restProps}>
-    {!buttonTitle ? (
-      <Header
-        title={title}
-        to={to}
-        href={href}
-        target={target}
-        onClick={onClick}
-        description={description}
-        {...titleStyle}
-      />
-    ) : (
-      <Header title={title} description={description} {...titleStyle} />
-    )}
-    {children}
-    {buttonTitle && (
-      <Footer buttonTitle={buttonTitle} to={to} href={href} target={target} onClick={onClick} {...buttonStyle} />
-    )}
-  </Section>
-);
+export default class Section extends PureComponent<Props> {
+  public render() {
+    const {
+      title,
+      titleStyle,
+      description,
+      buttonTitle,
+      buttonStyle,
+      to,
+      href,
+      target,
+      onClick,
+      children,
+      className,
+      divAttributes,
+    } = this.props;
+    return (
+      <section className={className} {...divAttributes}>
+        {!buttonTitle ? (
+          <Header
+            title={title}
+            to={to}
+            href={href}
+            target={target}
+            onClick={onClick}
+            description={description}
+            {...titleStyle}
+          />
+        ) : (
+          <Header title={title} description={description} {...titleStyle} />
+        )}
+        {children}
+        {buttonTitle && (
+          <Footer buttonTitle={buttonTitle} to={to} href={href} target={target} onClick={onClick} {...buttonStyle} />
+        )}
+      </section>
+    );
+  }
+}
