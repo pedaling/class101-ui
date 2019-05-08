4234dabc7af7486196a6bb5290c1887316761c25
chiabi <chiabi88@gmail.com>
2019-05-08 13:39:39 +0900

    feat: 브랜드 아이콘을 추가한다

--- docs/components/IconItemContainer.tsx
+++ docs/components/IconItemContainer.tsx
@@ -4,6 +4,19 @@ import styled from 'styled-components';
 
 import IconItem from './IconItem';
 
+const BLACK_LIST = [
+  'Apple',
+  'Facebook',
+  'Instagram',
+  'GooglePlaystore',
+  'GooglePlus',
+  'Kakaotalk',
+  'Naver',
+  'NaverTalk',
+  'NavertalkSymbol',
+  'Youtube',
+];
+
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -13,9 +26,11 @@ export default class IconItemContainer extends PureComponent<{}> {
   public render() {
     return (
       <Container>
-        {Object.keys(Icon).map(key => (
-          <IconItem iconName={key} />
-        ))}
+        {Object.keys(Icon)
+          .filter(key => !BLACK_LIST.includes(key))
+          .map(key => (
+            <IconItem iconName={key} />
+          ))}
       </Container>
     );
   }
--- docs/components/interface.ts
+++ docs/components/interface.ts
@@ -1,4 +1,3 @@
-// @flow
 export const imageSrc = 'https://cdn.class101.net/images/1fe7b071-44ab-4e20-a2d4-29e11b56384c/510x380.jpg';
 
 export default null;
