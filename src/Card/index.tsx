9dd781bd90079992a7e7b092331d9bfe07778e87
chiabi <chiabi88@gmail.com>
2019-05-09 22:41:15 +0900

    feat(deprecated): KlassCard를 제거한다
    - Card 컴포넌트를 PureComponent로 리팩토링한다

--- src/Card/index.tsx
+++ src/Card/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Link } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 
@@ -6,20 +6,117 @@ import { gray800 } from '../Colors';
 import { body2 } from '../TextStyles';
 
 interface Props {
+  /** 카드 타이틀 */
   title: string;
+
+  /** 카드 이미지 */
   coverImage: string | React.ReactNode;
+
+  /** 카드 srcSet */
   coverImageSrcSet?: string;
+
+  /** 대체 텍스트 */
   coverImageAlt?: string;
-  coverImageRatio?: '1*1' | '4*3' | '16*9';
+
+  /** 카드 이미지 비율 */
+  coverImageRatio: '1*1' | '4*3' | '16*9';
+
+  /** 타이틀 위에 들어갈 추가 노드 */
   extraTop?: React.ReactNode;
+
+  /** 타이틀 아래에 들어갈 추가 노드 */
   extraBottom?: React.ReactNode;
+
+  /** Link(React Router Dom)에 쓸 URL */
   to?: string;
+
+  /**	Native Anchor에 쓸 URL */
   href?: string;
+
+  className?: string;
   children?: React.ReactNode;
   target?: string;
 }
 
-const Card = styled.div`
+export default class Card extends PureComponent<Props> {
+  public static defaultProps: Partial<Props> = {
+    coverImageRatio: '4*3',
+  };
+
+  public render() {
+    const {
+      title,
+      coverImage,
+      coverImageSrcSet,
+      coverImageAlt,
+      coverImageRatio,
+      extraTop,
+      extraBottom,
+      children,
+      to,
+      href,
+      target,
+      className,
+    } = this.props;
+
+    const options: { rel?: string } = {};
+
+    if (target === '_blank') {
+      options.rel = 'noopener noreferrer';
+    }
+
+    const imgElements = () => {
+      if (typeof coverImage === 'string') {
+        return coverImageSrcSet ? (
+          <img src={coverImage} alt={coverImageAlt || ''} srcSet={coverImageSrcSet} />
+        ) : (
+          <img src={coverImage} alt={coverImageAlt || ''} />
+        );
+      }
+      return coverImage;
+    };
+
+    const innerElements = (
+      <>
+        <CoverImage coverImageRatio={coverImageRatio}>{imgElements()}</CoverImage>
+        <Body>
+          {extraTop}
+          <Title>{title}</Title>
+          {extraBottom}
+        </Body>
+      </>
+    );
+
+    if (to) {
+      return (
+        <Container className={className}>
+          <LinkCardInner to={to} target={target} {...options}>
+            {innerElements}
+          </LinkCardInner>
+          {children}
+        </Container>
+      );
+    }
+    if (href) {
+      return (
+        <Container className={className}>
+          <AnchorCardInner href={href} target={target} {...options}>
+            {innerElements}
+          </AnchorCardInner>
+          {children}
+        </Container>
+      );
+    }
+    return (
+      <Container className={className}>
+        {innerElements}
+        {children}
+      </Container>
+    );
+  }
+}
+
+const Container = styled.div`
   position: relative;
   width: 100%;
 `;
@@ -82,73 +179,3 @@ const LinkCardInner = styled(Link)`
 const AnchorCardInner = styled.a`
   ${anchorCardStyle};
 `;
-
-export default ({
-  title,
-  coverImage,
-  coverImageSrcSet,
-  coverImageAlt,
-  coverImageRatio = '4*3',
-  extraTop,
-  extraBottom,
-  children,
-  to,
-  href,
-  target,
-  ...restProps
-}: Props) => {
-  const options: { rel?: string } = {};
-
-  if (target === '_blank') {
-    options.rel = 'noopener noreferrer';
-  }
-
-  const imgElements = () => {
-    if (typeof coverImage === 'string') {
-      return coverImageSrcSet ? (
-        <img src={coverImage} alt={coverImageAlt || ''} srcSet={coverImageSrcSet} />
-      ) : (
-        <img src={coverImage} alt={coverImageAlt || ''} />
-      );
-    }
-    return coverImage;
-  };
-
-  const innerElements = (
-    <>
-      <CoverImage coverImageRatio={coverImageRatio}>{imgElements()}</CoverImage>
-      <Body>
-        {extraTop}
-        <Title>{title}</Title>
-        {extraBottom}
-      </Body>
-    </>
-  );
-
-  if (to) {
-    return (
-      <Card>
-        <LinkCardInner to={to} target={target} {...options}>
-          {innerElements}
-        </LinkCardInner>
-        {children}
-      </Card>
-    );
-  }
-  if (href) {
-    return (
-      <Card>
-        <AnchorCardInner href={href} target={target} {...options}>
-          {innerElements}
-        </AnchorCardInner>
-        {children}
-      </Card>
-    );
-  }
-  return (
-    <Card {...restProps}>
-      {innerElements}
-      {children}
-    </Card>
-  );
-};
