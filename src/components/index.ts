1e28704034b4837adae032deabc12621be5daf49
chiabi <chiabi88@gmail.com>
2019-06-23 17:20:45 +0900

    feat: callout 컴포넌트를 추가한다

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
+export * from './Callout';
+export { default as Callout } from './Callout';
+export * from './Carousel';
+export { CarouselNavigationPosition } from './Carousel/Navigation';
+export { default as Carousel } from './Carousel';
 export * from './Tag';
 export { default as Tag } from './Tag';
 export * from './Navigation';
 export { default as Navigation } from './Navigation';
-export * from './Carousel';
-export { CarouselNavigationPosition } from './Carousel/Navigation';
-export { default as Carousel } from './Carousel';
