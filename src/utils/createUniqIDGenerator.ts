96504bc2391dd286afd43dc0c2086e7f7fe8a153
chiabi <chiabi88@gmail.com>
2019-07-03 20:50:21 +0900

    refactor: generateID 함수를 수정한다
    - 전역 count를 사용하는 함수 대신 createUniqIDGenerator로 컴포넌트 사용때 generateID 함수를 생성하게 한다

---
+++ src/utils/createUniqIDGenerator.ts
@@ -0,0 +1,7 @@
+export default function createUniqIDGenerator(prefix: string) {
+  let count = 0;
+  return () => {
+    count = count + 1;
+    return `${prefix}${count}`;
+  };
+}
--- src/utils/generateID.ts
+++
@@ -1,6 +0,0 @@
-let generateCount = 0;
-
-export default function generateID(prefix = '') {
-  generateCount = generateCount + 1;
-  return `${prefix}${generateCount}`;
-}
