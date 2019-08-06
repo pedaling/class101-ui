dd105a0d1f4d82516bf0ce6d46d2d023dcccf542
chiabi <chiabi88@gmail.com>
2019-08-06 16:54:09 +0900

    feat: 버튼의 컬러를 추가한다
    - Button: blue, blue-right
    - TextButton: blue, white

--- src/components/Button/interface.ts
+++ src/components/Button/interface.ts
@@ -6,13 +6,24 @@ export enum ButtonSize {
   XSMALL = 'xs',
 }
 
-export type ButtonColorValue = 'default' | 'orange' | 'orangeLight' | 'red' | 'redLight' | 'white';
+export type ButtonColorValue =
+  | 'default'
+  | 'orange'
+  | 'orangeLight'
+  | 'red'
+  | 'redLight'
+  | 'white'
+  | 'blue'
+  | 'blueLight';
+
 export enum ButtonColor {
   DEFAULT = 'default',
   ORANGE = 'orange',
   ORANGE_LIGHT = 'orangeLight',
   RED = 'red',
   RED_LIGHT = 'redLight',
+  BLUE = 'blue',
+  BLUE_LIGHT = 'blueLight',
   WHITE = 'white',
 }
 
@@ -25,11 +36,13 @@ export enum TextButtonSize {
   SMALL = 'sm',
 }
 
-export type TextButtonColorValue = 'default' | 'orange' | 'red';
+export type TextButtonColorValue = 'default' | 'orange' | 'red' | 'white' | 'blue';
 export enum TextButtonColor {
   DEFAULT = 'default',
   ORANGE = 'orange',
   RED = 'red',
+  BLUE = 'blue',
+  WHITE = 'white',
 }
 
 export type ExcludeDefaultTextButtonColor = Exclude<TextButtonColor, TextButtonColor.DEFAULT>;
