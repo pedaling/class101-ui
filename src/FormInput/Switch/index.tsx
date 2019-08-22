26c271796a0366653a2ec0f0862a1e447d0d7840
tbvjaos510 <peter@class101.net>
2019-08-22 15:33:38 +0900

    feat: switch with text 제작

--- src/FormInput/Switch/index.tsx
+++ src/FormInput/Switch/index.tsx
@@ -1,51 +1,101 @@
-import React, { PureComponent, ChangeEvent } from 'react';
+import React, { PureComponent, ChangeEvent, ChangeEventHandler } from 'react';
 import styled, { css } from 'styled-components';
 import Theme, { ThemeConfig } from '../../Theme';
-import { gray100, orange500, gray300 } from '../../Colors';
+import { gray100, orange500, gray300, gray500, orange700, gray800 } from '../../Colors';
 import { elevation1 } from '../../ElevationStyles';
-
-// export type SwitchButtonSize = 'md' | 'lg';
+import { body2 } from '../../TextStyles';
 
 export interface SwitchProps {
-  // NOTE(peter): 일단 사이즈는 없애고 나중에 작업하기로 함.
-  // size: SwitchButtonSize;
+  className: string;
   theme: ThemeConfig;
   disabled: boolean;
   checked: boolean;
   color: string;
-  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  hoverColor: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => boolean | void;
+  inline?: boolean;
+  onText?: React.ReactNode;
+  offText?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
-export default class Switch extends PureComponent<SwitchProps> {
+export interface SwitchState {
+  checked: boolean;
+}
+
+export default class Switch extends PureComponent<SwitchProps, SwitchState> {
   public static defaultProps: Partial<SwitchProps> = {
     theme: Theme.light,
     disabled: false,
+    checked: false,
     color: orange500,
+    hoverColor: orange700,
+  };
+
+  private changeChecked: ChangeEventHandler<HTMLInputElement> = e => {
+    const { onChange } = this.props;
+    if (!onChange || onChange(e) !== false)
+      this.setState({
+        checked: e.target.checked,
+      });
+  };
+
+  public state: SwitchState = {
+    checked: this.props.checked,
   };
 
   public render() {
-    const { checked, ...restProps } = this.props;
+    const { inline, onText, offText, children, className, disabled, color, hoverColor } = this.props;
+    const { checked } = this.state;
+    const textExists = children || onText || offText;
     return (
-      <StyledSwitchBase>
-        <StyledSwitchInput type="checkbox" defaultChecked={checked} {...restProps} />
-      </StyledSwitchBase>
+      <StyledSwitchContainer inline={inline} disabled={disabled} className={className}>
+        {textExists && (
+          <StyledSwitchText inline={inline} disabled={disabled}>
+            {children || (checked ? onText : offText)}
+          </StyledSwitchText>
+        )}
+        <StyledSwitchBase>
+          <StyledSwitchInput
+            type="checkbox"
+            checked={checked}
+            onChange={this.changeChecked}
+            color={color}
+            hoverColor={hoverColor}
+            disabled={disabled}
+          />
+        </StyledSwitchBase>
+      </StyledSwitchContainer>
     );
   }
 }
 
-const switcherStyleBySize = css`
+const switcherSize = css`
   width: 36px;
   height: 22px;
 `;
 
 const StyledSwitchBase = styled.div`
-  ${switcherStyleBySize};
+  ${switcherSize};
+  display: flex;
   position: relative;
+`;
+
+const StyledSwitchContainer = styled.div<{ inline?: boolean; disabled: boolean }>`
+  display: ${props => (props.inline ? 'inline-flex' : 'flex')};
+  align-items: center;
+`;
+
+const StyledSwitchText = styled.span<{ disabled: boolean; inline?: boolean }>`
+  ${body2};
+  color: ${props => (props.disabled ? gray300 : gray800)};
+  margin-right: 9px;
+  ${props => !props.inline && `flex: 1 1 auto;`}
 `;
 
 const transitionAnimation = '0.15s ease-out 0.08s';
 
-const StyledSwitchInput = styled.input<{ disabled: boolean; color: string }>`
+const StyledSwitchInput = styled.input<{ disabled: boolean; color: string; hoverColor: string }>`
   box-sizing: border-box;
   appearance: none;
   height: 0;
@@ -61,18 +111,31 @@ const StyledSwitchInput = styled.input<{ disabled: boolean; color: string }>`
     content: '';
     position: absolute;
     box-sizing: border-box;
+
+    ${props =>
+      !props.disabled &&
+      `
     cursor: pointer;
+    `}
   }
 
   &:before {
     border-radius: 23px;
-    background: ${props => (props.disabled ? gray100 : gray300)};
 
     left: -2px;
     top: -2px;
 
     transition: background-color ${transitionAnimation};
-    ${switcherStyleBySize};
+    ${switcherSize};
+
+    ${props =>
+      props.disabled
+        ? `
+      background: ${gray100};
+      `
+        : `
+      background: ${gray300};
+    `}
   }
 
   &:after {
@@ -85,14 +148,29 @@ const StyledSwitchInput = styled.input<{ disabled: boolean; color: string }>`
     width: 18px;
     height: 18px;
   }
+
   &:checked {
     &:after {
       transform: translate(14px, 0);
     }
+    &:before {
+      background: ${props => props.color};
+      box-sizing: border-box;
+    }
   }
 
-  &:checked:before {
-    background: ${props => props.color};
-    box-sizing: border-box;
+  ${props =>
+    !props.disabled &&
+    `
+  &:hover {
+    &:before {
+      background: ${gray500}
+    }
+    &:checked {
+      &:before {
+        background: ${props.hoverColor}
+      }
+    }
   }
+  `}
 `;
