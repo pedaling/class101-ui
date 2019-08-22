3866d7104edfbd47e9b610f00fb6b0d13b11e74a
tbvjaos510 <peter@class101.net>
2019-08-22 16:42:57 +0900

    feat: stepper 추가

--- src/FormInput/Stepper/index.tsx
+++ src/FormInput/Stepper/index.tsx
@@ -9,25 +9,41 @@ import { HTMLInputProps } from 'interfaces/props';
 
 interface Props {
   size: InputSize;
+  value: number;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  min?: number;
+  max?: number;
+  step: number;
 }
 
-export default class Stepper extends PureComponent<HTMLInputProps & Props> {
-  public static defaultProps: Props = {
+interface State {
+  value: number;
+}
+
+export default class Stepper extends PureComponent<Props, State> {
+  public static defaultProps: Partial<Props> = {
     size: InputSize.md,
+    value: 0,
+    step: 1,
+  };
+
+  public state: State = {
+    value: this.props.value,
   };
 
   public render() {
-    const { size, ...inputProps } = this.props;
+    const { size, value: _, ...inputProps } = this.props;
+    const { value } = this.state;
     return (
       <StyledStepperGroup size={size} fill>
-        {size === InputSize.sm && (
-          <StyledStepperIconButton icon={<MinusCircle />} color="transparent" fillColor={gray300} marginLeft />
-        )}
-        <StyledStepperInput align={size === 'md' ? 'left' : 'center'} {...inputProps} type="number" />
-        {size === InputSize.md && (
-          <StyledStepperIconButton icon={<MinusCircle />} color="transparent" fillColor={gray300} marginRight />
-        )}
-        <StyledStepperIconButton icon={<AddCircleOutline />} color="transparent" marginRight />
+        <StyledStepperInput align={size === 'md' ? 'left' : 'center'} value={value} {...inputProps} type="number" />
+        <StyledStepperIconButton
+          icon={<MinusCircle />}
+          color="transparent"
+          fillColor={gray300}
+          order={size === 'sm' ? -1 : 0}
+        />
+        <StyledStepperIconButton icon={<AddCircleOutline />} color="transparent" />
       </StyledStepperGroup>
     );
   }
@@ -37,7 +53,7 @@ const StyledStepperGroup = styled(ControlGroup)<{ size: InputSize }>`
   ${FormInputStyle};
   width: ${props => (props.size === InputSize.md ? 312 : 148)}px;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   border-radius: 3px;
 `;
 
@@ -53,9 +69,12 @@ const StyledStepperInput = styled(Input)<{ align: 'left' | 'center' | 'right' }>
     margin: 0;
   }
   padding: 0;
+  ${props => props.align === 'left' && 'padding-left: 18px;'}
 `;
 
-const StyledStepperIconButton = styled(IconButton)<{ marginRight?: boolean; marginLeft?: boolean }>`
-  ${props => props.marginRight && `margin-right: 18px;`}
-  ${props => props.marginLeft && `margin-left: 18px;`}
+const StyledStepperIconButton = styled(IconButton)<{
+  order?: number;
+}>`
+  flex: 0;
+  ${props => (props.order ? `order:${props.order};margin-left: 18px;` : `margin-right: 18px`)}
 `;
