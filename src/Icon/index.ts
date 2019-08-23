ded97c0250cca4101ec21099cd0e5ce91e6692b1
yozzing <ddalpange@gmail.com>
2019-08-23 18:50:56 +0900

    feat: 페이지네이션 완성

--- src/Icon/index.ts
+++ src/Icon/index.ts
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 export interface IconProps {
@@ -26,6 +25,8 @@ export { default as CheckboxIndeterminated } from './generated/CheckboxIndetermi
 export { default as CheckboxOff } from './generated/CheckboxOff';
 export { default as CheckboxOn } from './generated/CheckboxOn';
 export { default as Chevron } from './generated/Chevron';
+export { default as ChevronRight } from './generated/Chevron'; // 일관성을 위해 다시 익스포트
+export { default as ChevronLeft } from './generated/ChevronLeft';
 export { default as ChevronDown } from './generated/ChevronDown';
 export { default as Clip } from './generated/Clip';
 export { default as Clock } from './generated/Clock';
