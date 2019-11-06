7074a4d88b79f8a5ebb5465addd41554bdc1454b
john015 <lsw0150305@gmail.com>
2019-11-06 15:15:24 +0900

    feat: add ifDarkTheme util

---
+++ src/utils/ifDarkTheme.ts
@@ -0,0 +1,10 @@
+import { FlattenSimpleInterpolation } from 'styled-components';
+import { ThemeMode } from '../core';
+
+interface Theme {
+  mode: ThemeMode.DARK | ThemeMode.LIGHT;
+}
+
+export const ifDarkTheme = (darkThemeStyle: string | FlattenSimpleInterpolation) => ({ theme }: { theme: Theme }) => {
+  return theme.mode === ThemeMode.DARK ? darkThemeStyle : '';
+};
