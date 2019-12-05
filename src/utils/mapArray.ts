c2960d9347b5b2ddde76576b5fbce4834e82236c
chihye <chiabi88@gmail.com>
2019-12-05 18:40:03 +0900

    Revert "feat: GridListCompnent에 generic을 추가한다"
    This reverts commit 0476227e329636b07e8f9e923d6c1db80db8c66d.

--- src/utils/mapArray.ts
+++
@@ -1,6 +0,0 @@
-export function mapArray<T, U>(
-  array: T[] | ReadonlyArray<T>,
-  callbackfn: (value: T, index: number, array: ReadonlyArray<T>) => U
-) {
-  return (array as T[]).map(callbackfn);
-}
