1a95e332c5819362435181269d01d1538c5acad2
lonyele <skyrabbits1@gmail.com>
2020-02-05 15:13:58 +0900

    feat(ui): Pagination에 많은 데이터가 들어왔을때에 적당한 대응 가능하게 수정

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -17,25 +17,45 @@ export class Pagination extends PureComponent<PaginationProps> {
     const handleIncrease = this.handleChange(currentPageIndex + 1);
     const handleDecrease = this.handleChange(currentPageIndex - 1);
 
-    const indexList = range(0, totalPageIndex + 1);
+    const indexList = this.getIndexList(currentPageIndex, totalPageIndex);
 
     return (
       <PageWrapper>
         <PageIconBtn icon={<ChevronLeft />} onClick={handleDecrease} disabled={currentPageIndex <= 0} />
-        {indexList.map((_, i) => (
+        {indexList.map(index => (
           <PageBtn
-            color={i === currentPageIndex ? ButtonColor.ORANGE : ButtonColor.WHITE}
+            color={index === currentPageIndex ? ButtonColor.ORANGE : ButtonColor.WHITE}
             size="sm"
-            onClick={this.handleChange(i)}
-            key={`pagination-${i}`}
+            onClick={this.handleChange(index)}
+            key={`pagination-${index}`}
           >
-            {i + 1}
+            {index + 1}
           </PageBtn>
         ))}
         <PageIconBtn icon={<ChevronRight />} onClick={handleIncrease} disabled={currentPageIndex >= totalPageIndex} />
       </PageWrapper>
     );
   }
+
+  private getIndexList = (currentPageIndex: number, totalPageIndex: number) => {
+    // 보여줄수 있는거 다 보여주기
+    if (totalPageIndex < 5) {
+      return range(0, totalPageIndex + 1);
+    }
+
+    // 맨 앞에서부터 5개 보여주기
+    if (currentPageIndex - 2 <= 0) {
+      return range(0, 5);
+    }
+
+    // 맨 뒤에서부터 5개 보여주기
+    if (currentPageIndex + 2 >= totalPageIndex) {
+      return range(totalPageIndex - 4, totalPageIndex + 1);
+    }
+
+    // currentPageIndex와 앞뒤 2개씩 보여주기
+    return range(currentPageIndex - 2, currentPageIndex + 3);
+  };
 
   private handleChange = (pageIndex: number) => () => {
     const { onChange } = this.props;
