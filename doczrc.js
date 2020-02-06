bd34c2220e882e9029aa1fb5442412fd63f8a7eb
배현승 <bae-unidev@nate.com>
2020-02-06 14:51:12 +0900

    docs(ui): 내용을 영문으로 바꾸고, 어색한 스타일을 정리한다

--- doczrc.js
+++ doczrc.js
@@ -1,7 +1,7 @@
 import path from 'path';
 
 export default {
-  title: 'Class101 UI Docs',
+  title: 'Class101 UI',
   description: 'A React-based UI Component Library, powered by Class101.',
   src: './docs/',
   dest: './public',
@@ -12,7 +12,20 @@ export default {
     colors: {
       primary: '#fd7e14',
     },
+    styles: {
+      body: {
+        fontFamily:
+          '-apple-system, "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Open Sans", "Helvetica Neue", "Icons16", sans-serif',
+      },
+      h1: {
+        fontFamily: 'inherit',
+        fontWeight: 600,
+        lineHeight: '60px',
+        fontSize: 40,
+      },
+    },
   },
+  menu: ['Class101 UI', 'Core', 'Components', 'Form Controls', 'Form Inputs', 'Overlays'],
   wrapper: 'docs/Wrapper',
   typescript: true,
   modifyBundlerConfig: config => ({
