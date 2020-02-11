41e53bbe9258e5d54dc401e5c5bf9d8baada1551
john015 <lsw0150305@gmail.com>
2020-02-11 15:35:43 +0900

    feat(ui): Icon.category 컴포넌트의 아이콘을 변경한다

--- src/Icon/generated/Category.tsx
+++ src/Icon/generated/Category.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React from 'react';
 import { IconProps } from '../index';
 export default class SvgCategory extends React.PureComponent<IconProps> {
   public static defaultProps: Partial<IconProps> = {
     fillColor: '#3E4042',
     accentColor: '#DDE0E2',
-    size: 24
+    size: 24,
   };
 
   public render() {
-    return <svg width={this.props.size} height={this.props.size} style={this.props.style} className={this.props.className} viewBox="0 0 24 24"><path fill={this.props.fillColor} fillRule="evenodd" d="M3 5h18v2H3V5zm0 6h18v2H3v-2zm0 6h18v2H3v-2z" /></svg>;
+    return (
+      <svg
+        width={this.props.size}
+        height={this.props.size}
+        fill="none"
+        style={this.props.style}
+        className={this.props.className}
+        viewBox="0 0 24 24"
+      >
+        <path fillRule="evenodd" clipRule="evenodd" d="M0 2h16V0H0v2zm0 6h16V6H0v2zm0 6h16v-2H0v2z" fill="#1B1C1D" />
+      </svg>
+    );
   }
-
 }
