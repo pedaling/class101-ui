143b3e4c821bc45a5d898e01769f498b19048205
jeffchoi72 <ihello0720@gmail.com>
2020-02-22 17:16:54 +0900

    chore: 아이콘을 업데이트한다

--- src/Icon/generated/Play.tsx
+++ src/Icon/generated/Play.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React from 'react';
 import { IconProps } from '../index';
 export default class SvgPlay extends React.PureComponent<IconProps> {
   public static defaultProps: Partial<IconProps> = {
     fillColor: '#3E4042',
     accentColor: '#DDE0E2',
-    size: 24
+    size: 24,
   };
 
   public render() {
-    return <svg width={this.props.size} height={this.props.size} style={this.props.style} className={this.props.className} viewBox="0 0 24 24"><path fill={this.props.fillColor} fillRule="evenodd" d="M5 19.29V4.71a1 1 0 011.49-.872l12.96 7.29a1 1 0 010 1.744l-12.96 7.29A1 1 0 015 19.29z" /></svg>;
+    return (
+      <svg
+        width={this.props.size}
+        height={this.props.size}
+        fill="none"
+        style={this.props.style}
+        className={this.props.className}
+        viewBox="0 0 24 24"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M5.498 3.135a1 1 0 01.998-.003l14 8a1 1 0 010 1.736l-14 8A1 1 0 015 20V4a1 1 0 01.498-.865z"
+          fill={this.props.fillColor}
+        />
+      </svg>
+    );
   }
-
 }
