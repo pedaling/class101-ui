dee5b12699d09672d44e7321b8f600049fcee68c
yozzing <ddalpange@gmail.com>
2020-02-25 14:31:48 +0900

    fix: 컬러 안먹던거 수정
    - lightTheme의 defaultColor를 gray000으로 변경
    - 코드 리팩터링
    - 시멘틱에 맞게 hr로 변경

--- src/layout/Divider/index.tsx
+++ src/layout/Divider/index.tsx
@@ -1,31 +1,18 @@
 import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 
-import { Theme, ThemeConfig, ThemeMode } from '../../core';
-import { gray200, gray800 } from '../../core/Colors';
+import { gray000, gray800 } from '../../core/Colors';
+import { ifDarkTheme } from '../../utils';
 
 interface Props {
   className?: string;
   color?: string;
-  width: string;
-  theme: ThemeConfig;
+  width?: string;
 }
 
 export class Divider extends PureComponent<Props> {
-  public static defaultProps: Partial<Props> = {
-    width: '100%',
-    theme: Theme.light,
-  };
-
-  private get color() {
-    const { color, theme } = this.props;
-    return color || theme.mode === ThemeMode.LIGHT ? gray200 : gray800;
-  }
-
   public render() {
-    const { color } = this;
-    const { className, width } = this.props;
-
+    const { className, width, color } = this.props;
     return (
       <Container className={className} width={width}>
         <Line color={color} />
@@ -34,12 +21,12 @@ export class Divider extends PureComponent<Props> {
   }
 }
 
-const Container = styled.div<{ width: string }>`
-  width: ${props => props.width};
+const Container = styled.div<Pick<Props, 'width'>>`
+  ${props => (props.width ? `width: ${typeof props.width === 'number' ? `${props.width}px` : props.width}` : '')};
 `;
 
-const Line = styled.div<{ color: string }>`
-  width: 100%;
+const Line = styled.hr<{ color?: string }>`
+  box-shadow: inset 0px -1px 0px ${props => props.color ?? ifDarkTheme(gray800, gray000)};
+  border: none;
   height: 1px;
-  box-shadow: inset 0px -1px 0px ${props => props.color};
 `;
