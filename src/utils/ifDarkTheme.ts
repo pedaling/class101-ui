015f48f745314d4a142e24eae64928c8759d6015
yozzing <ddalpange@gmail.com>
2020-02-25 14:31:19 +0900

    feat: add fallbackStyle in ifDarkTheme

--- src/utils/ifDarkTheme.ts
+++ src/utils/ifDarkTheme.ts
@@ -1,10 +1,14 @@
 import { FlattenSimpleInterpolation } from 'styled-components';
+
 import { ThemeMode } from '../core';
 
 interface Theme {
   mode: ThemeMode.DARK | ThemeMode.LIGHT;
 }
 
-export const ifDarkTheme = (darkThemeStyle: string | FlattenSimpleInterpolation) => ({ theme }: { theme: Theme }) => {
-  return theme.mode === ThemeMode.DARK ? darkThemeStyle : '';
+export const ifDarkTheme = (
+  darkThemeStyle?: string | FlattenSimpleInterpolation,
+  fallBackStyle: string | FlattenSimpleInterpolation = ''
+) => ({ theme }: { theme: Theme }) => {
+  return theme.mode === ThemeMode.DARK ? darkThemeStyle : fallBackStyle;
 };
