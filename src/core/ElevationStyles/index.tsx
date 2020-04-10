dbadaedef254a5fc78dfa8d7709fbb019dee39db
Churros <churros@class101.net>
2020-04-10 17:58:03 +0900

    feat(ui) elevation 변경 사항을 적용한다. (#229)

--- src/core/ElevationStyles/index.tsx
+++ src/core/ElevationStyles/index.tsx
@@ -7,29 +7,29 @@ export interface ElevationProps {
 }
 
 export const elevation1 = css`
-  box-shadow: 0px 1px 3px rgba(41, 42, 43, 0.08), 0px 0.5px 0px rgba(0, 0, 0, 0.04);
+  box-shadow: 0px 1px 3px rgba(41, 42, 43, 0.1), 0px 0px 0.5px rgba(0, 0, 0, 0.04);
 `;
 
 export const elevation2 = css`
-  box-shadow: 0px 2px 6px rgba(41, 42, 43, 0.08), 0px 1px 0px rgba(41, 42, 43, 0.04);
+  box-shadow: 0px 2px 5px rgba(41, 42, 43, 0.1), 0px 0px 0.5px rgba(41, 42, 43, 0.06);
 `;
 
 export const elevation3 = css`
-  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.01), 0px 2px 2.5px rgba(0, 0, 0, 0.02), 0px 4.5px 5px rgba(0, 0, 0, 0.03),
-    0px 9px 9px rgba(0, 0, 0, 0.04);
+  box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.02), 0px 2px 5px rgba(0, 0, 0, 0.03), 0px 3px 7px rgba(0, 0, 0, 0.04),
+    0px 7px 10px rgba(0, 0, 0, 0.04);
 `;
 
 export const elevation4 = css`
-  box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.02), 0px 6px 6px rgba(0, 0, 0, 0.03), 0px 8px 12px rgba(0, 0, 0, 0.05),
-    0px 14px 18px rgba(0, 0, 0, 0.06);
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.02), 0px 3px 6px rgba(0, 0, 0, 0.03), 0px 8px 12px rgba(0, 0, 0, 0.05),
+    0px 12px 18px rgba(0, 0, 0, 0.06);
 `;
 
 export const elevation5 = css`
-  box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.02), 0px 6px 4px rgba(0, 0, 0, 0.02), 0px 9px 6px rgba(0, 0, 0, 0.03),
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.02), 0px 6px 4px rgba(0, 0, 0, 0.02), 0px 9px 6px rgba(0, 0, 0, 0.03),
     0px 16px 12px rgba(0, 0, 0, 0.03), 0px 24px 24px rgba(0, 0, 0, 0.08);
 `;
 
 export const elevation6 = css`
-  box-shadow: 0px 8px 6px rgba(0, 0, 0, 0.01), 0px 12px 9px rgba(0, 0, 0, 0.02), 0px 16px 12px rgba(0, 0, 0, 0.025),
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.02), 0px 6px 9px rgba(0, 0, 0, 0.02), 0px 16px 12px rgba(0, 0, 0, 0.025),
     0px 26px 24px rgba(0, 0, 0, 0.06), 0px 28px 32px rgba(0, 0, 0, 0.08);
 `;
