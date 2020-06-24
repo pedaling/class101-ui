cfebb8ba3edef11b6f30d613c010d86702b2a80e
churros <churros@class101.net>
2020-06-24 17:03:42 +0900

    feat(web): tooltip 색상을 추가한다.

--- src/overlays/Tooltip/colors.ts
+++ src/overlays/Tooltip/colors.ts
@@ -1,16 +1,19 @@
-import { gray100, gray900, orange100, orange600 } from '../../core/Colors';
+import { gray100, gray900, orange100, orange600, red400 } from '../../core/Colors';
 
 export enum TooltipColor {
   ORANGE,
+  HOTPINK,
   DEFAULT,
 }
 
 export const tooltipBackgroundColors = {
   [TooltipColor.ORANGE]: orange600,
+  [TooltipColor.HOTPINK]: red400,
   [TooltipColor.DEFAULT]: gray900,
 };
 
 export const tooltipColors = {
   [TooltipColor.DEFAULT]: gray100,
+  [TooltipColor.HOTPINK]: gray100,
   [TooltipColor.ORANGE]: orange100,
 };
