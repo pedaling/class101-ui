1e2b6f2f6b9bbe774c1b7546f86a7867fe6f35cb
chichi <chiabi88@gmail.com>
2020-07-03 17:13:51 +0900

    feat: swiper navigation 스타일 오버라이딩이 적용되지 않는 문제를 수정한다

--- src/components/Swiper/ArrowNavigation/index.tsx
+++ src/components/Swiper/ArrowNavigation/index.tsx
@@ -1,47 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { ChevronRight } from '../../../Icon';
+import { ChevronLeft, ChevronRight } from '../../../Icon';
 
 export const ArrowNavigation = () => {
   return (
     <>
-      <Button className="swiper-button-prev" direction="left">
-        <ChevronRight fillColor="white" />
+      <Button className="swiper-button-prev">
+        <ChevronLeft fillColor="white" />
       </Button>
-      <Button className="swiper-button-next" direction="right">
+      <Button className="swiper-button-next">
         <ChevronRight fillColor="white" />
       </Button>
     </>
   );
 };
 
-const Button = styled.button<{ direction: 'right' | 'left' }>`
-  background: none;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0;
-  position: absolute;
-  width: 48px;
-  margin-top: 0;
-  top: 0;
-  height: 100%;
-  border: 0;
-  background-image: linear-gradient(90deg, rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.15));
-  ${props =>
-    props.direction === 'right'
-      ? `
-      right: 0;
-  `
-      : `
-      left: 0;
-      transform: rotate(-180deg);
-  `};
+const Button = styled.button`
+  &.swiper-button-prev,
+  &.swiper-button-next {
+    background: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0;
+    position: absolute;
+    width: 48px;
+    margin-top: 0;
+    top: 0;
+    height: 100%;
+    border: 0;
+  }
+
+  &.swiper-button-prev {
+    left: 0;
+    background-image: linear-gradient(90deg, rgba(0, 0, 0, 0.15), rgba(255, 255, 255, 0));
+  }
+
+  &.swiper-button-next {
+    right: 0;
+    background-image: linear-gradient(90deg, rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.15));
+  }
+
   &.swiper-button-disabled {
     pointer-events: auto;
     cursor: not-allowed;
   }
+
   svg {
     flex: none;
   }
