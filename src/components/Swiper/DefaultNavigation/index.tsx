6501eecc606052a8b062db2eb4f749e79e236f39
chichi <chiabi88@gmail.com>
2020-07-03 17:13:51 +0900

    feat: swiper navigation 스타일 오버라이딩이 적용되지 않는 문제를 수정한다

--- src/components/Swiper/DefaultNavigation/index.tsx
+++ src/components/Swiper/DefaultNavigation/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { elevation1 } from '../../../core/ElevationStyles';
-import { ChevronRight } from '../../../Icon';
+import { ChevronLeft, ChevronRight } from '../../../Icon';
 import { ButtonColor, IconButton } from '../../Button';
 
 const NAVIGATION_BUTTON_SIZE = 32;
@@ -11,33 +11,16 @@ export type NavigationDirection = 'right' | 'left';
 
 export const DefaultNavigation = () => {
   return (
-    <Container className="swiper-default-navigation">
-      <Button color={ButtonColor.WHITE} className="swiper-button-prev" direction="left" icon={<ChevronRight />} />
-      <Button color={ButtonColor.WHITE} className="swiper-button-next" direction="right" icon={<ChevronRight />} />
-    </Container>
+    <div className="swiper-default-navigation">
+      <Button color={ButtonColor.WHITE} className="swiper-button-prev" icon={<ChevronLeft />} />
+      <Button color={ButtonColor.WHITE} className="swiper-button-next" icon={<ChevronRight />} />
+    </div>
   );
 };
 
-const Button = styled(IconButton)<{ direction: 'right' | 'left' }>`
-  ${props =>
-    props.direction === 'right'
-      ? `
-      right: 0;
-      border-top-left-radius: 0;
-      border-bottom-left-radius: 0;
-    `
-      : `
-      right: ${NAVIGATION_BUTTON_SIZE}px;
-      border-top-right-radius: 0;
-      border-bottom-right-radius: 0;
-      svg {
-        transform: rotate(180deg);
-      }
-`};
-`;
-
-const Container = styled.div`
-  > ${Button} {
+const Button = styled(IconButton)`
+  &.swiper-button-prev,
+  &.swiper-button-next {
     ${elevation1};
     width: 32px;
     height: 32px;
@@ -54,5 +37,17 @@ const Container = styled.div`
       opacity: 1;
       pointer-events: auto;
     }
+  }
+
+  &.swiper-button-prev {
+    right: ${NAVIGATION_BUTTON_SIZE}px;
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+
+  &.swiper-button-next {
+    right: 0;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
   }
 `;
