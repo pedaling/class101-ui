e5d33b7e9e67e98e401d5725550c1658f9ffb026
HowToBeAHappyBoy <seozi@101.inc>
2021-03-05 15:55:01 +0900

    chore: 다크모드 색상을 추가한다

---
+++ src/core/DarkColors/index.ts
@@ -0,0 +1,102 @@
+export const black = '#000000';
+export const white = '#ffffff';
+
+export const gray100 = '#080808';
+export const gray200 = '#141414';
+export const gray300 = '#191919';
+export const gray400 = '#2a2a2a';
+export const gray500 = '#373737';
+export const gray600 = '#4c4c4c';
+export const gray700 = '#858585';
+export const gray800 = '#bbbbbb';
+export const gray900 = '#f6f6f6';
+
+export const orange100 = '#6f1400';
+export const orange200 = '#ae1f00';
+export const orange300 = '#ec2a00';
+export const orange400 = '#ff3d00';
+export const orange500 = '#ff540c';
+export const orange600 = '#ff6111';
+export const orange700 = '#ff722a';
+export const orange800 = '#ffb894';
+export const orange900 = '#ffe5d1';
+
+export const pinkRed100 = '#660012';
+export const pinkRed200 = '#a80f2b';
+export const pinkRed300 = '#d4092d';
+export const pinkRed400 = '#f51631';
+export const pinkRed500 = '#fd3049';
+export const pinkRed600 = '#ff3e55';
+export const pinkRed700 = '#ff525b';
+export const pinkRed800 = '#ffa5a9';
+export const pinkRed900 = '#ffd4d7';
+
+export const magenta100 = '#53002d';
+export const magenta200 = '#a8005b';
+export const magenta300 = '#b90064';
+export const magenta400 = '#cc006e';
+export const magenta500 = '#ec0d85';
+export const magenta600 = '#f52e99';
+export const magenta700 = '#ff47aa';
+export const magenta800 = '#ffb8dd';
+export const magenta900 = '#fee7f3';
+
+export const yellow100 = '#6d3b00';
+export const yellow200 = '#c77800';
+export const yellow300 = '#ee9d00';
+export const yellow400 = '#ffc700';
+export const yellow500 = '#ffd90f';
+export const yellow600 = '#ffdd29';
+export const yellow700 = '#ffe457';
+export const yellow800 = '#ffec89';
+export const yellow900 = '#fff8d1';
+
+export const violetBlue100 = '#1c358d';
+export const violetBlue200 = '#3859cc';
+export const violetBlue300 = '#4466de';
+export const violetBlue400 = '#5277f7';
+export const violetBlue500 = '#6b8cff';
+export const violetBlue600 = '#99afff';
+export const violetBlue700 = '#bccbff';
+export const violetBlue800 = '#dae2ff';
+export const violetBlue900 = '#e9edff';
+
+export const blue100 = '#003e78';
+export const blue200 = '#0065c0';
+export const blue300 = '#0074dd';
+export const blue400 = '#1a88eb';
+export const blue500 = '#329eff';
+export const blue600 = '#6cb8fc';
+export const blue700 = '#a3d3ff';
+export const blue800 = '#bcdfff';
+export const blue900 = '#ddefff';
+
+export const aquaGreen100 = '#00555a';
+export const aquaGreen200 = '#008f98';
+export const aquaGreen300 = '#009aa3';
+export const aquaGreen400 = '#00b0bb';
+export const aquaGreen500 = '#0ad1df';
+export const aquaGreen600 = '#51dee7';
+export const aquaGreen700 = '#6ee9f2';
+export const aquaGreen800 = '#9ff2f8';
+export const aquaGreen900 = '#cdfcff';
+
+export const seaGreen100 = '#003e35';
+export const seaGreen200 = '#007061';
+export const seaGreen300 = '#028572';
+export const seaGreen400 = '#00ad94';
+export const seaGreen500 = '#03cfb2';
+export const seaGreen600 = '#08e5c6';
+export const seaGreen700 = '#50f3dc';
+export const seaGreen800 = '#94f8ea';
+export const seaGreen900 = '#d2fff9';
+
+export const green100 = '#00380f';
+export const green200 = '#007520';
+export const green300 = '#019d2c';
+export const green400 = '#03bb37';
+export const green500 = '#09dc44';
+export const green600 = '#19e351';
+export const green700 = '#42e770';
+export const green800 = '#7cf29d';
+export const green900 = '#c9f6d6';
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,15 +1,17 @@
 import * as breakPoints from './BreakPoints';
 import * as colors from './Colors';
+import * as darkColors from './DarkColors';
 import * as elevationStyles from './ElevationStyles';
 import * as textStyles from './TextStyles';
 
 // MEMO: import 한 값을 변수로 할당 안 하고 바로 export할 경우 esm format 빌드할 때 rollup이 index로 이름을 바꿔버려서 output이 이상해지기 때문에 변수로 할당 한다.
 const BreakPoints = breakPoints;
 const Colors = colors;
+const DarkColors = darkColors;
 const ElevationStyles = elevationStyles;
 const TextStyles = textStyles;
 
-export { BreakPoints, Colors, TextStyles, ElevationStyles };
+export { BreakPoints, Colors, DarkColors, TextStyles, ElevationStyles };
 
 export * from './GlobalStyle';
 export * from './Theme';
