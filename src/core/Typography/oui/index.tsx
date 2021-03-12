2232c708a6e662728eb0333249ce28e7e50e3f28
Seozi <seozi@101.inc>
2021-03-12 15:59:39 +0900

    chore: OUI TextStyle을 추가한다 (#321)
    * chore: OUI TextStyle을 추가한다
    
    * fix: 안 쓰는 import 제거

---
+++ src/core/Typography/oui/index.tsx
@@ -0,0 +1,123 @@
+import React, { PureComponent } from 'react';
+import { TypoProps, BaseTypography, HeadlineTypoProps } from '../BaseTypography';
+
+export class OUIBody1 extends PureComponent<TypoProps> {
+  public render() {
+    const { children, ...restProps } = this.props;
+    return (
+      <BaseTypography md="OUIBody1" {...restProps}>
+        {children}
+      </BaseTypography>
+    );
+  }
+}
+
+export class OUIBody2 extends PureComponent<TypoProps> {
+  public render() {
+    const { children, ...restProps } = this.props;
+    return (
+      <BaseTypography md="OUIBody2" {...restProps}>
+        {children}
+      </BaseTypography>
+    );
+  }
+}
+
+export class OUIBody3 extends PureComponent<TypoProps> {
+  public render() {
+    const { children, ...restProps } = this.props;
+    return (
+      <BaseTypography md="OUIBody3" {...restProps}>
+        {children}
+      </BaseTypography>
+    );
+  }
+}
+
+export class OUICaption1 extends PureComponent<TypoProps> {
+  public render() {
+    const { children, ...restProps } = this.props;
+    return (
+      <BaseTypography md="OUICaption1" {...restProps}>
+        {children}
+      </BaseTypography>
+    );
+  }
+}
+
+export class OUICaption2 extends PureComponent<TypoProps> {
+  public render() {
+    const { children, ...restProps } = this.props;
+    return (
+      <BaseTypography md="OUICaption2" {...restProps}>
+        {children}
+      </BaseTypography>
+    );
+  }
+}
+
+export class OUICaption3 extends PureComponent<TypoProps> {
+  public render() {
+    const { children, ...restProps } = this.props;
+    return (
+      <BaseTypography md="OUICaption3" {...restProps}>
+        {children}
+      </BaseTypography>
+    );
+  }
+}
+
+export class OUIHeadline1 extends PureComponent<HeadlineTypoProps> {
+  public render() {
+    const { children, ...restProps } = this.props;
+    return (
+      <BaseTypography md="OUIHeadline1" {...restProps}>
+        {children}
+      </BaseTypography>
+    );
+  }
+}
+
+export class OUIHeadline2 extends PureComponent<HeadlineTypoProps> {
+  public render() {
+    const { children, ...restProps } = this.props;
+    return (
+      <BaseTypography md="OUIHeadline2" {...restProps}>
+        {children}
+      </BaseTypography>
+    );
+  }
+}
+
+export class OUIHeadline3 extends PureComponent<HeadlineTypoProps> {
+  public render() {
+    const { children, ...restProps } = this.props;
+    return (
+      <BaseTypography md="OUIHeadline3" {...restProps}>
+        {children}
+      </BaseTypography>
+    );
+  }
+}
+
+export class OUISubtitle1 extends PureComponent<TypoProps> {
+  public render() {
+    const { children, ...restProps } = this.props;
+    return (
+      <BaseTypography md="OUISubtitle1" {...restProps}>
+        {children}
+      </BaseTypography>
+    );
+  }
+}
+
+export class OUISubtitle2 extends PureComponent<TypoProps> {
+  public render() {
+    const { children, ...restProps } = this.props;
+    return (
+      <BaseTypography md="OUISubtitle2" {...restProps}>
+        {children}
+      </BaseTypography>
+    );
+  }
+}
