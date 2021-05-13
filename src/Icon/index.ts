7191ea7a1c30d6c28f264f232d587452e6224c41
Naki <nbsp1221@gmail.com>
2021-05-13 13:19:39 +0900

    style: 코드 리뷰를 반영하여 `deprecated`를 추가한다

--- src/Icon/index.ts
+++ src/Icon/index.ts
@@ -3,5 +3,8 @@ import * as Icon from './export-legacy.generated';
 export * from './export.generated';
 export * from './types';
 
-// MEMO(Naki, @nbsp1221): 기존 코드와의 호환성을 위해 레거시 버전을 고려함
+/**
+ * MEMO(Naki, @nbsp1221): 기존 코드와의 호환성을 위해 레거시 버전을 고려함
+ * @deprecated 트리 쉐이킹 문제로 사용하시면 안됩니다.
+ */
 export { Icon };
