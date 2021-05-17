c04f43a9261a4b757868497e5fe131dc3d70d38a
Naki <nbsp1221@gmail.com>
2021-05-17 12:15:45 +0900

    style: 기존 아이콘 사용 방식에 `@deprecated` 처리를 한다 (#341)
    * style: 기존 아이콘 사용 방식에 `@deprecated` 처리를 한다
    
    * Fix style

--- src/Icon/index.ts
+++ src/Icon/index.ts
@@ -1,10 +1,21 @@
-import * as Icon from './export-legacy.generated';
+import * as LegacyIcon from './export-legacy.generated';
 
 export * from './export.generated';
 export * from './types';
 
 /**
- * MEMO(Naki, @nbsp1221): 기존 코드와의 호환성을 위해 레거시 버전을 고려함
- * @deprecated 트리 쉐이킹 문제로 사용하시면 안됩니다.
+ * 트리 쉐이킹(Tree Shaking) 문제로 아이콘 사용 방법을 변경합니다.
+ * 기존 방식은 호환성을 위해 남겨두었습니다.
+ *
+ * @author Naki(nbsp1221)
+ * @deprecated 이 방식은 더 이상 사용되지 않습니다. 새로운 방식의 아이콘을 사용해 주세요.
+ * @example
+ *   // Before
+ *   import { Icon } from '@class101/ui';
+ *   const IconComponent = () => <Icon.Add />;
+ *
+ *   // After
+ *   import { AddIcon } from '@class101/ui';
+ *   const IconComponent = () => <AddIcon />;
  */
-export { Icon };
+export const Icon = LegacyIcon;
