1f1623b30d07ef890282070fbc461457203db632
neroli-dev <neroli@hobbyinthebox.co.kr>
2021-06-02 16:35:54 +0900

    fix docz

--- src/formControls/index.ts
+++ src/formControls/index.ts
@@ -1,8 +1,9 @@
 import { RadioButton } from './RadioButtonGroup/RadioButton';
 import type { RadioButtonProps } from './RadioButtonGroup/RadioButton';
+import { RadioButtonGroup } from './RadioButtonGroup';
+import type { RadioButtonGroupProps } from './RadioButtonGroup';
 
 export * from './ControlGroup';
 export * from './FormGroup';
-export * from './RadioButtonGroup';
-export { RadioButton };
-export type { RadioButtonProps };
+export { RadioButton, RadioButtonGroup };
+export type { RadioButtonProps, RadioButtonGroupProps };
